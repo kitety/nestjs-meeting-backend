a8f116a343e250217de21c0bb2a9c45fe5e19bc3
kitety <aizaizuori@foxmail.com>
2024-05-28 17:33:39 +0800

    feat: 鉴权完善

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RequireLogin, RequirePermission, UserInfo } from './custom.decorator';
+import { JwtUserData } from './login.guard';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,19 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('aaa')
+  // @SetMetadata('require-login', true)
+  // @SetMetadata('require-permission', ['ddd'])
+  @RequireLogin()
+  @RequirePermission('ddd')
+  aaaa(@UserInfo() user: JwtUserData) {
+    return user;
+  }
+
+  @Get('bbb')
+  bbb() {
+    return 'bbb';
   }
 }
--- src/permission.guard.ts
+++ src/permission.guard.ts
@@ -1,11 +1,43 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+import { Request } from 'express';
 
 @Injectable()
 export class PermissionGuard implements CanActivate {
+  @Inject(Reflector)
+  private reflector: Reflector;
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+    // 强制登陆了才会有user对象 @SetMetadata('require-login', true)
+    if (!request.user) return true;
+
+    const permissions = request.user.permissions;
+    const requiredPermissions = this.reflector.getAllAndOverride<string[]>(
+      'require-permission',
+      [context.getClass(), context.getHandler()],
+    );
+    if (!requiredPermissions) {
+      return true;
+    }
+
+    const hasPermission = requiredPermissions.every((permission) =>
+      permissions.some((userPermission) => userPermission.code === permission),
+    );
+
+    if (!hasPermission) {
+      throw new UnauthorizedException('您没有访问该接口的权限');
+    }
+
     return true;
   }
 }
